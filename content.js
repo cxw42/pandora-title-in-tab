61fe069f98d55a632d78d9d33db04f3e4a7e9199
Chris White <chrisw@leehayes.com>
2019-02-21 13:26:51 -0500

    Also show whether you thumbed up the current track
    So you don't click back to the browser to thumb it up, only to see
    that you already did so!

--- content.js
+++ content.js
@@ -7,17 +7,33 @@ console.log('Pandora title in tab loading');
 
 function update() {
     'use strict';
-    var track='', artist='';
-    var elems = document.getElementsByClassName('Tuner__Audio__TrackDetail__title');
-    if(elems.length === 1) { track = elems[0].textContent; }
-    
-    elems = document.getElementsByClassName('Tuner__Audio__TrackDetail__artist');
-    if(elems.length === 1) { artist = elems[0].textContent; }
 
-    var newtitle = `${track} - ${artist} - Pandora`;
-    if(newtitle !== document.title) {
-        document.title = newtitle;
-    }
+    try {   // Ignore all fatal errors
+        var track='', artist='', thumb='';
+        var elems = document.getElementsByClassName('Tuner__Audio__TrackDetail__title');
+        if(elems.length === 1) { track = elems[0].textContent; }
+
+        elems = document.getElementsByClassName('Tuner__Audio__TrackDetail__artist');
+        if(elems.length === 1) { artist = elems[0].textContent; }
+
+        // Is it thumbed up?
+
+        try {   // Ignore all errors
+            var thumb_snap = document.evaluate('//button[@data-qa="thumbs_up_button"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE);
+            if(thumb_snap && thumb_snap.snapshotLength >=1) {
+                var thumb_btn = thumb_snap.snapshotItem(0);
+                if(thumb_btn.classList.contains('ThumbUpButton--active')) {
+                    thumb = String.fromCodePoint(0x1f44d) + ' ';
+                                    // U+1F44D THUMBS UP SIGN
+                }
+            }
+        } catch { }
+
+        var newtitle = `${thumb}${track} - ${artist} - Pandora`;
+        if(newtitle !== document.title) {
+            document.title = newtitle;
+        }
+    } catch { }
 
     setTimeout(update, 5000);
 } //update()
